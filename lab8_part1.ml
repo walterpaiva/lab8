d7427893006da3c936df2c63b2927d3339d639e2
walternpaiva <walternpaiva@gmail.com>
2020-02-28 15:57:36 -0500

    submission2

--- lab8_part1.ml
+++ lab8_part1.ml
@@ -112,12 +112,12 @@ module MakeInterval (Endpoint : ORDERED_TYPE) =
     (* intersect intvl1 intvl2 -- Returns the intersection of `intvl1`
        and `intvl2` *)
     let intersect (intvl1 : interval) (intvl2 : interval) : interval =
-      match intvl1, intvl2 with
-      | Empty, Empty -> Empty
-      | Empty, Interval (a1, b1) -> intvl2
-      | Interval (a1, b1), Empty -> intvl1 
-      | Interval (a1, b1), Interval (a2, b2) ->
-      Interval ((if compare a1 a2 > 0 then a2 else a1), (if compare b1 b2 > 0 then b1 else b2))
+      let ordered x y = if Endpoint.compare x y <= 0 then x, y else y, x in
+        match intvl1, intvl2 with
+        | Empty, _
+        | _, Empty -> Empty
+        | Interval (low1, high1), Interval (low2, high2) ->
+        let (_, low), (high, _)  = ordered low1 low2, ordered high1 high2 in create low high
     end ;;
 
 (*......................................................................
